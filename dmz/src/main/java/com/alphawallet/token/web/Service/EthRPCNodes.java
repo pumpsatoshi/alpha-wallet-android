f50285923376ecfec243f915f7885d06e1b1bd67
Nurkhalis Patulungi <patulungik@gmail.com>
2023-07-20 19:21:33 +0700

    Update EthRPCNodes.java

--- dmz/src/main/java/com/alphawallet/token/web/Service/EthRPCNodes.java
+++ dmz/src/main/java/com/alphawallet/token/web/Service/EthRPCNodes.java
@@ -8,7 +8,7 @@ public class EthRPCNodes
 {
     private static final String MAINNET_RPC_URL = "https://mainnet.infura.io/v3/" + getInfuraKey();
     private static final String CLASSIC_RPC_URL = "https://www.ethercluster.com/etc";
-    private static final String XDAI_RPC_URL = EthereumNetworkBase.XDAI_RPC_URL;
+    private static final String XTUPLE_RPC_URL = EthereumNetworkBase.XTUPLE_RPC_URL;
     private static final String GOERLI_RPC_URL = "https://goerli.infura.io/v3/" + getInfuraKey();
 
 
@@ -18,8 +18,8 @@ public class EthRPCNodes
                 return MAINNET_RPC_URL;
             case (int)EthereumNetworkBase.CLASSIC_ID:
                 return CLASSIC_RPC_URL;
-            case (int)EthereumNetworkBase.GNOSIS_ID:
-                return XDAI_RPC_URL;
+            case (int)EthereumNetworkBase.XTUPLE_ID:
+                return XTUPLE_RPC_URL;
             case (int)EthereumNetworkBase.GOERLI_ID:
                 return GOERLI_RPC_URL;
             default:
